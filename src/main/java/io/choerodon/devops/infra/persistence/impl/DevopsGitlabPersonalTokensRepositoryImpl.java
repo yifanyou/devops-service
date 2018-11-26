a0ec6b53f29f0691370d56d982a54968c27211ff
mumutu <luye66@163.com>
2018-11-26 23:06:13 +0800

    fix

--- src/main/java/io/choerodon/devops/infra/persistence/impl/DevopsGitlabPersonalTokensRepositoryImpl.java
+++ src/main/java/io/choerodon/devops/infra/persistence/impl/DevopsGitlabPersonalTokensRepositoryImpl.java
@@ -5,12 +5,12 @@ import io.choerodon.devops.domain.application.repository.GitlabRepository;
 import io.choerodon.devops.infra.dataobject.DevopsGitlabPersonalTokensDO;
 import io.choerodon.devops.infra.mapper.DevopsGitlabPersonalTokensMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Component
+@Service
 public class DevopsGitlabPersonalTokensRepositoryImpl implements DevopsGitlabPersonalTokensRepository {
 
     @Autowired
